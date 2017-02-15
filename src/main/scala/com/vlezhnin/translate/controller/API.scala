77ad57858147e71a84cdc471129c7fbfc39e906e
Vlad Lezhnin <vle@tradeshift.com>
2017-02-16 00:10:04 +0100

    add default second language

--- src/main/scala/com/vlezhnin/translate/controller/API.scala
+++ src/main/scala/com/vlezhnin/translate/controller/API.scala
@@ -10,6 +10,8 @@ class API extends Controller {
 
   val YANDEX_API_KEY = "trnsl.1.1.20161026T190949Z.7010d055d88c34c4.068b7c9f6a931ea34df15a6f7f6660a24ba36e97"
 
+  val DEFAULT_SECOND_LANGUAGE = "ru"
+
   val yandexApiClient: Service[Request, Response] = Http.client.withTls("translate.yandex.net").newService("translate.yandex.net:443")
 
   val danskOrdbogClient: Service[Request, Response] = Http.client.newService("ordnet.dk:80")
@@ -20,10 +22,11 @@ class API extends Controller {
 
   post("/rest/translate") { request: Request =>
     val text: String = request.getParam("text")
+    val secondLang: String = request.getParam("lang", DEFAULT_SECOND_LANGUAGE)
     println(s"Request received with query $text")
 
-    val yandexRequestEn = Request(com.twitter.finagle.http.Method.Get, "/api/v1.5/tr.json/translate?key=" + YANDEX_API_KEY + "&lang=da-en&text=" + text)
-    val yandexRequestRu = Request(com.twitter.finagle.http.Method.Get, "/api/v1.5/tr.json/translate?key=" + YANDEX_API_KEY + "&lang=da-ru&text=" + text)
+    val yandexRequestEn = Request(com.twitter.finagle.http.Method.Get, s"/api/v1.5/tr.json/translate?key=$YANDEX_API_KEY&lang=da-en&text=$text")
+    val yandexRequestRu = Request(com.twitter.finagle.http.Method.Get, s"/api/v1.5/tr.json/translate?key=$YANDEX_API_KEY&lang=da-$secondLang&text=$text")
 
     val yandexResponseEn: Future[Response] = yandexApiClient(yandexRequestEn)
     val yandexResponseRu: Future[Response] = yandexApiClient(yandexRequestRu)
